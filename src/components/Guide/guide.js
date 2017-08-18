e18a142a62ef13c89d8e5a04f28be76917d13002
lucy.chu0632 <lucy.chu0632@gmail.com>
2017-08-18 14:51:43 +0800

    add guide

---
+++ src/components/Guide/guide.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Guide extends Component {
+  render() {
+    return (
+      <div className="guide">
+        <h1>guide</h1>
+      </div>
+    );
+  }
+}
+
+export default Guide;
